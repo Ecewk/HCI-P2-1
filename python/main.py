df6bfb6b77f32d3a388a7ae5ce6f4bca22d1e892
Dimonnnnchik <ananaskorea96@gmail.com>
2024-11-21 20:58:08 +0100

    fish ai added, view frustrum, listener, copied python files from Mika, scenes template

---
+++ python/main.py
@@ -0,0 +1,41 @@
+import cv2
+import socket
+from handTracker import handTrackerClass
+from eyeTracker import eyeTrackerClass
+
+
+if __name__ == "__main__":    
+    # Host and port for the TCP connection
+    host, port = "127.0.0.1", 25001
+
+    # Initialize the TCP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("Start up hand tracking")
+    myHandtracker = handTrackerClass(sock)
+    myEyetracker = eyeTrackerClass(sock)
+
+    try:
+        # Connect to the server
+        sock.connect((host, port))
+
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, img = cap.read()
+            if not ret:
+                break
+
+            myHandtracker.recog_gestures(img)
+            #myEyetracker.send_eyes_position(img)
+
+            img = cv2.flip(img, 1)
+            #cv2.imshow("Image", img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    
+    except socket.error:
+        print("Start unity project")
+
+    finally:
+        # Release resources
+        sock.close()
