cac57fe01057eba7bafd0a792b60baba2a3d6812
mlwauben <mika.wauben@gmail.com>
2025-01-09 13:54:36 +0100

    add mainmanager

---
+++ ParallaxWorld/Assets/Scripts/MainManager.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using System.Threading;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+
+    Thread thread;
+    public int port = 25001;
+    private UdpClient udpServer;
+    private IPEndPoint remoteEndPoint;
+    bool positionUpdated = false;
+    public bool isInitialized = false;
+    public Vector3 currentUserPos;
+    public Vector3 lastUserPos;
+    public string SceneName = "";
+    public GameObject target;
+    public Camera cam;
+
+    private static MainManager mainManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartServer();
+        Debug.Log($"TESTTTT");
+    }
+
+    void StartServer()
+    {
+        thread = new Thread(new ThreadStart(StartUDPServer));
+        thread.IsBackground = true;
+        thread.Start();
+    }
+
+    private void StartUDPServer()
+    {
+        udpServer = new UdpClient(port);
+        remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+        Debug.Log("Server started. Waiting for messages...");
+
+        // Start receiving data asynchronously
+        udpServer.BeginReceive(ReceiveData, null);
+    }
+
+    private void ReceiveData(IAsyncResult result)
+    {
+        byte[] receivedBytes = udpServer.EndReceive(result, ref remoteEndPoint);
+        string receivedMessage = System.Text.Encoding.UTF8.GetString(receivedBytes);
+
+        Debug.Log("Received from client: " + receivedMessage);
+
+        // Process the received data
+        if (receivedMessage.Contains(",")) {
+            Vector3 position = ParseData(receivedMessage);
+            //TODO check if this has to be removed
+            lock(this)
+            {
+                currentUserPos = position;
+                positionUpdated = true;
+            }
+        } else {
+            SceneName = receivedMessage;
+        }
+
+
+        // Continue receiving data asynchronously
+        udpServer.BeginReceive(ReceiveData, null);
+    } 
+
+    Vector3 ParseData(string data)
+    {
+        string[] parts = data.Split(',');
+        if (parts.Length == 3)
+        {
+            float x = float.Parse(parts[0]);
+            float y = float.Parse(parts[1]); //inverted y axis
+            float z = float.Parse(parts[2]);
+
+            if(!isInitialized)
+            {
+                currentUserPos = new Vector3(x, y, z);
+                lastUserPos = currentUserPos;
+                isInitialized = true;
+                return currentUserPos;
+            }
+            return new Vector3(x, y, z);
+        }
+        return Vector3.zero;
+    }
+
+    void Awake()
+    {
+        if (mainManager == null) {
+            mainManager = this;
+            DontDestroyOnLoad(mainManager);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.W) || SceneName == "main") && (SceneManager.GetActiveScene().name == "FinalJungle" || SceneManager.GetActiveScene().name == "FinalWater")) {
+            SceneManager.LoadScene("FinalMain");
+            //TODO fix target and cam referencing
+            target = GameObject.Find("mainTarget");
+            cam = Camera.main;
+        }
+        if ((Input.GetKeyDown(KeyCode.A) || SceneName == "jungle") && (SceneManager.GetActiveScene().name == "FinalMain")) {
+            SceneManager.LoadScene("FinalJungle");
+            target = GameObject.Find("jungleTarget");
+            cam = Camera.main;
+        }
+        if ((Input.GetKeyDown(KeyCode.D) || SceneName == "water") && (SceneManager.GetActiveScene().name == "FinalMain")) {
+            SceneManager.LoadScene("FinalWater");
+            target = GameObject.Find("waterTarget");
+            cam = Camera.main;
+        }
+
+        float speed = 0.5F;
+        cam.transform.RotateAround(target.transform.position, cam.transform.right, -Input.GetAxis("Mouse Y") * speed);
+        cam.transform.RotateAround(target.transform.position, cam.transform.up, -Input.GetAxis("Mouse X") * speed);
+    }
+}
