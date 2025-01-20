34357aaab585d1047c076a70782e3ef79646aca2
Dimonnnnchik <ananaskorea96@gmail.com>
2025-01-20 21:34:28 +0100

    changed  or fixed fps, added sceneonload tracking

--- ParallaxWorld/Assets/Scripts/MainManager.cs
+++ ParallaxWorld/Assets/Scripts/MainManager.cs
@@ -9,7 +9,6 @@ using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour
 {
-
     Thread thread;
     public int port = 25001;
     private UdpClient udpServer;
@@ -23,10 +22,31 @@ public class MainManager : MonoBehaviour
     public Camera cam;
 
     private static MainManager mainManager;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private float startTime;
+    private string filePath = "sceneLoadTimes.txt";
+
     void Start()
     {
         StartServer();
+
+        // Clear the file at the start (optional)
+        System.IO.File.WriteAllText(filePath, "Scene Load Times:\n");
+
+        // Subscribe to scene loaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDestroy()
+    {
+        // Unsubscribe to avoid memory leaks
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+
+        // Clean up the thread
+        if (thread != null && thread.IsAlive)
+        {
+            thread.Abort();
+        }
     }
 
     void StartServer()
@@ -50,27 +70,27 @@ public class MainManager : MonoBehaviour
     private void ReceiveData(IAsyncResult result)
     {
         byte[] receivedBytes = udpServer.EndReceive(result, ref remoteEndPoint);
-        string receivedMessage = System.Text.Encoding.UTF8.GetString(receivedBytes);
+        string receivedMessage = Encoding.UTF8.GetString(receivedBytes);
 
         Debug.Log("Received from client: " + receivedMessage);
 
-        // Process the received data
-        if (receivedMessage.Contains(",")) {
+        if (receivedMessage.Contains(","))
+        {
             Vector3 position = ParseData(receivedMessage);
-            //TODO check if this has to be removed
-            lock(this)
+            lock (this)
             {
                 currentUserPos = position;
                 positionUpdated = true;
             }
-        } else {
+        }
+        else
+        {
             SceneName = receivedMessage;
         }
 
-
         // Continue receiving data asynchronously
         udpServer.BeginReceive(ReceiveData, null);
-    } 
+    }
 
     Vector3 ParseData(string data)
     {
@@ -78,10 +98,10 @@ public class MainManager : MonoBehaviour
         if (parts.Length == 3)
         {
             float x = float.Parse(parts[0]);
-            float y = float.Parse(parts[1]); //inverted y axis
+            float y = float.Parse(parts[1]);
             float z = float.Parse(parts[2]);
 
-            if(!isInitialized)
+            if (!isInitialized)
             {
                 currentUserPos = new Vector3(x, y, z);
                 lastUserPos = currentUserPos;
@@ -95,36 +115,55 @@ public class MainManager : MonoBehaviour
 
     void Awake()
     {
-        if (mainManager == null) {
+        if (mainManager == null)
+        {
             mainManager = this;
             DontDestroyOnLoad(mainManager);
         }
-        else {
+        else
+        {
             Destroy(gameObject);
         }
-    } 
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.W) || SceneName == "main") && (SceneManager.GetActiveScene().name == "FinalJungle" || SceneManager.GetActiveScene().name == "FinalWater")) {
-            SceneManager.LoadScene("FinalMain");
-            target.transform.position = new Vector3(1.49999976F,21.2399998F,49.7000008F);
+        if ((Input.GetKeyDown(KeyCode.W) || SceneName == "main") && (SceneManager.GetActiveScene().name == "FinalJungle" || SceneManager.GetActiveScene().name == "FinalWater"))
+        {
+            LoadSceneWithTiming("FinalMain", new Vector3(1.49999976F, 21.2399998F, 49.7000008F));
         }
-        if ((Input.GetKeyDown(KeyCode.A) || SceneName == "jungle") && (SceneManager.GetActiveScene().name == "FinalMain")) {
-            SceneManager.LoadScene("FinalJungle");
-            target.transform.position = new Vector3(-0.300000191F,24.5400028F,117F);
+        if ((Input.GetKeyDown(KeyCode.A) || SceneName == "jungle") && (SceneManager.GetActiveScene().name == "FinalMain"))
+        {
+            LoadSceneWithTiming("FinalJungle", new Vector3(-0.300000191F, 24.5400028F, 117F));
         }
-        if ((Input.GetKeyDown(KeyCode.D) || SceneName == "water") && (SceneManager.GetActiveScene().name == "FinalMain")) {
-            SceneManager.LoadScene("FinalWater");
-            target.transform.position = new Vector3(-0.300000191F,13.7399998F,87.6999969F);
+        if ((Input.GetKeyDown(KeyCode.D) || SceneName == "water") && (SceneManager.GetActiveScene().name == "FinalMain"))
+        {
+            LoadSceneWithTiming("FinalWater", new Vector3(-0.300000191F, 13.7399998F, 87.6999969F));
         }
 
         float speed = 0.5F;
-        //cam.transform.RotateAround(target.transform.position, cam.transform.right, -Input.GetAxis("Mouse Y") * speed);
-        //cam.transform.RotateAround(target.transform.position, cam.transform.up, -Input.GetAxis("Mouse X") * speed);
-        cam.transform.RotateAround(target.transform.position, cam.transform.right, (currentUserPos.y - lastUserPos.y)/10 * speed);
-        cam.transform.RotateAround(target.transform.position, cam.transform.up, -(currentUserPos.x - lastUserPos.x)/10 * speed);
-        lastUserPos = currentUserPos; 
+        cam.transform.RotateAround(target.transform.position, cam.transform.right, (currentUserPos.y - lastUserPos.y) / 10 * speed);
+        cam.transform.RotateAround(target.transform.position, cam.transform.up, -(currentUserPos.x - lastUserPos.x) / 10 * speed);
+        lastUserPos = currentUserPos;
+    }
+
+    private void LoadSceneWithTiming(string sceneName, Vector3 targetPosition)
+    {
+        startTime = Time.realtimeSinceStartup;
+        SceneManager.LoadScene(sceneName);
+        target.transform.position = targetPosition;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        float loadTime = Time.realtimeSinceStartup - startTime;
+        Debug.Log($"Scene '{scene.name}' loaded in {loadTime} seconds.");
+        SaveLoadTime(scene.name, loadTime);
+    }
+
+    private void SaveLoadTime(string sceneName, float loadTime)
+    {
+        string logEntry = $"Scene '{sceneName}' loaded in {loadTime:F2} seconds.\n";
+        System.IO.File.AppendAllText(filePath, logEntry);
     }
 }
--- ParallaxWorld/Assets/Scripts/seeFps.cs
+++ ParallaxWorld/Assets/Scripts/seeFps.cs
@@ -2,24 +2,40 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-//to see the fps in the game
-public class showFPS : MonoBehaviour {
-	public TMPro.TextMeshProUGUI fpsText;
-	private float fps;
-	private string filePath = "fps.txt";
+public class ShowFPS : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI fpsText;
+    private float accumulatedDeltaTime = 0.0f;
+    private int frameCount = 0;
+    private string filePath = "fps.txt";
 
+    void Start()
+    {
+        System.IO.File.AppendAllText(filePath, "-------------\n");
+        InvokeRepeating("DisplayAndSaveFps", 1, 1);
+    }
 
-	void Start () {
-		System.IO.File.AppendAllText(filePath,"-------------\n");
-		InvokeRepeating("GetFps", 1, 1);
-	}
-	void GetFps(){
-		fps = (int)(1f / Time.unscaledDeltaTime);
-		fpsText.text = "FPS: " + fps.ToString();
-		saveFps();
+    void Update()
+    {
+        accumulatedDeltaTime += Time.unscaledDeltaTime;
+        frameCount++;
+    }
 
-	}
-	void saveFps(){
-		System.IO.File.AppendAllText(filePath, fps.ToString() + "\n");
-	}
+    void DisplayAndSaveFps()
+    {
+        float averageDeltaTime = accumulatedDeltaTime / frameCount;
+        float fps = 1f / averageDeltaTime;
+        fpsText.text = "FPS: " + Mathf.RoundToInt(fps).ToString();
+
+        saveFps(fps);
+
+        // Reset for the next interval
+        accumulatedDeltaTime = 0f;
+        frameCount = 0;
+    }
+
+    void saveFps(float fps)
+    {
+        System.IO.File.AppendAllText(filePath, Mathf.RoundToInt(fps).ToString() + "\n");
+    }
 }
